1dc543e4c456c554bdbd279a1f7ce5a791e13364
k1ngsterr1 <swerss564@gmail.com>
2026-01-14 23:30:20 +0500

    feat: integrate PostgreSQL adapter and add product images for iPhone 17 models

--- prisma/import-iphone17.ts
+++ prisma/import-iphone17.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import * as XLSX from 'xlsx';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -6,7 +8,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
 
 // Маппинг цветов к файлам изображений
 const colorImageMap: { [key: string]: string[] } = {
